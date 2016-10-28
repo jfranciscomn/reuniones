eb7ebe331c90e5d982f9627a2ffb9b5b9b6626b2
Roberto Zamarripa <roberto@masoft.mx>
2016-10-28 15:09:37 -0600

    reuniones y acuerdos participantes

--- client/scripts/controllers/nuevoAcuerdo.controller.js
+++ client/scripts/controllers/nuevoAcuerdo.controller.js
@@ -5,22 +5,61 @@ angular
 		
 		this.acuerdoId = $stateParams.acuerdoId;
 		this.acuerdo = Acuerdos.findOne(this.acuerdoId)
+		this.quitarhk=function(obj){
+		if(Array.isArray(obj)){
+			for (var i = 0; i < obj.length; i++) {
+				obj[i] =this.quitarhk(obj[i]);
+			}
+		}
+		else if(obj !== null && typeof obj === 'object')
+		{
+			delete obj.$$hashKey;
+			for (var name in obj) {
+	  			obj[name] = this.quitarhk(obj[name]);
+			}
+
+		}
+		return obj;
+	}
+	
+	if(!this.acuerdo){
+		this.acuerdo={users:[{user:Meteor.userId()}]};
+	}
 		
 		this.helpers({
 			categorias() {
 				return Categorias.find();
+			},
+			registrados() {
+				console.log("registrados", Meteor.users.find({}).fetch());
+				return Meteor.users.find({});
 			}
 		});
+		
+		this.agregarParticipante = function(){
+		
+			this.acuerdo.users.push({user:null});
+			console.log(this.acuerdo.users)
+		}
 
 
-	this.save = function(){
-		console.log(this.acuerdoId);
+		this.save  = function(){
+		this.quitarhk(this.acuerdo);
+
 		if(this.acuerdoId){
 			delete this.acuerdo._id
 			Acuerdos.update({_id:this.acuerdoId},{$set:this.acuerdo});
 		}
-		else
-			Acuerdos.insert(this.acuerdo);
+		else{
+			
+			this.acuerdo.createdAt = new Date();
+      		this.acuerdo.owner = Meteor.userId();
+      		//this.acuerdo.users =[{user:Meteor.userId()}];
+      		this.acuerdo.username = Meteor.user().username;
+      		Acuerdos.insert(this.acuerdo);
+      
+		}
+		console.log($ionicHistory)	
 		$ionicHistory.goBack();
 		
 	}
