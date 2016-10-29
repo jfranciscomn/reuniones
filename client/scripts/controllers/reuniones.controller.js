1ca30ba71c5fbb96fc051315c59e8c28ae7beffe
Roberto Zamarripa <roberto@masoft.mx>
2016-10-29 14:09:45 -0600

    menú en reuniones

--- client/scripts/controllers/reuniones.controller.js
+++ client/scripts/controllers/reuniones.controller.js
@@ -3,6 +3,7 @@ angular
   .controller('ReunionesCtrl', function AcuerdosCtrl($scope, $reactive, $state, $stateParams, $ionicPopup) {
 		$reactive(this).attach($scope);
 		this.contacts = [];
+		this.deviceWidth = window.screen.width;
 		this.helpers({
 			reuniones() {
 				return Reuniones.find({users:{ $elemMatch: {user:Meteor.userId()} }});
