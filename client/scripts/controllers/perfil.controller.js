c77f08be3f837ae4b595e69b9ee24d5f888c5488
Roberto Zamarripa <roberto@masoft.mx>
2016-10-31 20:24:24 -0700

    perfil y acuerdos

--- client/scripts/controllers/perfil.controller.js
+++ client/scripts/controllers/perfil.controller.js
@@ -1,20 +1,33 @@
 angular
   .module('FLOKsports')
-  .controller('PerfilCtrl', function AcuerdosCtrl($scope, $reactive, $state, $stateParams, $ionicPopup) {
-		$reactive(this).attach($scope);
+  .controller('PerfilCtrl', function AcuerdosCtrl($scope, $meteor, $reactive, $state, $stateParams, $ionicPopup) {
+		let rc = $reactive(this).attach($scope);
+		this.user = {};
+		this.editar = false;
+		this.helpers({
+	    user: function(){	      
+	      return Meteor.users.findOne(Meteor.userId());
+	    },
+	  });	  
 	
-		this.updatePicture = function () {
-	    MeteorCameraUI.getPicture({ width: 60, height: 60 }, (err, data) => {
-	      if (err) return this.handleError(err);
-	 
-	      this.$ionicLoading.show({
-	        template: 'Updating picture...'
-	      });
-	 
-	      this.callMethod('updatePicture', data, (err) => {
-	        this.$ionicLoading.hide();
-	        this.handleError(err);
-	      });
+		this.takePicture = function(){
+			this.editar = true;
+	    $meteor.getPicture({width:128, height: 128, quality: 100}).then(function(picture, ){
+	      rc.user.profile.picture = picture;
+	      Meteor.users.update({_id: rc.user._id}, {$set:{profile: rc.user.profile}});
+				console.log(rc.user);
 	    });
+
+	  };
+	  
+	  this.getPicture = function(){
+	    return rc.user.profile.picture;
 	  }
+	  
+	  this.actualizar = function(){
+	    Meteor.users.update({_id: rc.user._id}, {$set:{profile: rc.user.profile}});
+	    this.edit = false;
+	  };
 });
+
+// roberto@masoft.mx
