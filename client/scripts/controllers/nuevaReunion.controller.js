d7606c0a177a98a434a477ac725caf8c158e03ea
Roberto Zamarripa <roberto@masoft.mx>
2016-11-04 13:34:28 -0700

    participantes agregado

--- client/scripts/controllers/nuevaReunion.controller.js
+++ client/scripts/controllers/nuevaReunion.controller.js
@@ -1,28 +1,33 @@
 angular
   .module('FLOKsports')
-  .controller('NuevaReunionCtrl', function AcuerdosCtrl($scope, $reactive, $state, $stateParams, $ionicPopup, $ionicHistory) {
-		$reactive(this).attach($scope);
+  .controller('NuevaReunionCtrl', function AcuerdosCtrl($scope, $reactive, $state, $stateParams, $ionicPopup, $ionicHistory, $ionicModal) {
+		let rc = $reactive(this).attach($scope);
 		this.reunionId = $stateParams.reunionId;
 		this.reunion = Reuniones.findOne(this.reunionId);
+		this.buscar = "";
+		this.opcion = {};
+		this.opcion.participantes = [];
 		this.quitarhk=function(obj){
 			if(Array.isArray(obj)){
 				for (var i = 0; i < obj.length; i++) {
 					obj[i] =this.quitarhk(obj[i]);
 				}
 			}
-			else if(obj !== null && typeof obj === 'object')
-			{
+			else if(obj !== null && typeof obj === 'object'){
 				delete obj.$$hashKey;
 				for (var name in obj) {
 		  			obj[name] = this.quitarhk(obj[name]);
-				}
-	
+				}	
 			}
 			return obj;
 		}
 			
 		if(!this.reunion){
 			this.reunion={users:[{user:Meteor.userId(), estatus : 2}]};
+			this.reunion.createdAt = new Date();
+  		this.reunion.owner = (Meteor.userId() != undefined) ? Meteor.userId() : "";
+  		this.reunion.username = (Meteor.userId() != undefined) ? Meteor.user().username : "";
+  		this.reunion.estatus = 1;
 		}
 		this.helpers({
 			registrados() {
@@ -33,11 +38,11 @@ angular
 			}
 		});
 	
-		this.agregarParticipante = function(){
-			
-			this.reunion.users.push({user:null, estatus : 1});
-
+		this.agregarParticipante = function(participante){
+			console.log(participante);
+			this.reunion.users.push({user:participante._id, estatus : 1});
 		}
+		
 		this.save  = function(){
 			console.log(this.reunionId);
 			this.quitarhk(this.reunion);
@@ -48,13 +53,54 @@ angular
 				Reuniones.update({_id:this.reunionId},{$set:this.reunion});
 			}
 			else{
-				this.reunion.createdAt = new Date();
-    		this.reunion.owner = Meteor.userId();
-    		this.reunion.username = Meteor.user().username;
     		Reuniones.insert(this.reunion);
-	      
 			}
 			$ionicHistory.goBack();
-			
 		}
+		
+		$ionicModal.fromTemplateUrl('client/templates/participantes/modalSelParticipantes.html', {
+	    scope: $scope,
+	    animation: 'slide-in-up'
+	  }).then(function(modal) {
+	    $scope.modal = modal;
+	  });
+	  
+	  this.selParticipantes = function() {
+	    $scope.modal.show();
+	  };
+	  
+	  this.cerrarModalParticipantes = function() {
+	    $scope.modal.hide();
+	  };
+	  
+	  this.getConfirmados = function(usuarios){
+			var confirmados = 0;
+			_.each(usuarios, function(usuario){
+				if(usuario.estatus == 2){
+					confirmados++;
+				}
+			})
+			return confirmados;
+		}
+		
+		this.getPendientes = function(usuarios){
+			var pendientes = 0;
+			_.each(usuarios, function(usuario){
+				if(usuario.estatus == 1){
+					pendientes++;
+				}
+			})
+			return pendientes;
+		}
+		
+		this.getRechazados = function(usuarios){
+			var rechazados = 0;
+			_.each(usuarios, function(usuario){
+				if(usuario.estatus == 6){
+					rechazados++;
+				}
+			})
+			return rechazados;
+		}
+	  
 });
--- client/scripts/controllers/reunionesPorConfirmar.controller.js
+++ client/scripts/controllers/reunionesPorConfirmar.controller.js
@@ -12,24 +12,35 @@ angular
 		
 		this.cambiarEstatus = function(reunion, estatus){
 			var mensaje = "";
-			if(estatus == 1 )
-				mensaje = "pendiente";
-			else if(estatus == 2)
-				mensaje = "aceptada";
-			else if(estatus == 6)
-				mensaje = "rechazada"
+			var titulo = "";
+			if(estatus == 1 ){
+				mensaje = "Pendiente";
+				titulo = "Pendiente"
+			}				
+			else if(estatus == 2){
+				mensaje = "Aceptada";
+				titulo = "Aceptar";
+				reunion.estatus = 2;
+			}				
+			else if(estatus == 6){
+				mensaje = "Rechazada";
+				titulo = "Rechazar";
+			}
+				
 			var confirmPopup = $ionicPopup.confirm({
-				title: 'Confirmar',
+				title: titulo,
 				template: "Está seguro de poner como " + mensaje
 			});
 			
 			confirmPopup.then(function(res) {
-				if(res) {
+				if(res) { 
 					_.each(reunion.users, function(usuario){
 						if(usuario.user == Meteor.userId()){
-							Reuniones.update({ _id : reunion._id, 'users.user' : Meteor.userId() }, { $set : { "users.$.estatus" : estatus }});
+							console.log(estatus);
+							usuario.estatus = estatus;
 						}
 					})
+					Reuniones.update({ _id : reunion._id}, { $set : { users: reunion.users }});
 				}
 			});
 		}
